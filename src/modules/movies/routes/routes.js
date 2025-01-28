113aaa43b46efcc7e7ebf54e62e6b148af7a710d
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-28 18:19:46 +0800

    add search movie by title endpoint

--- src/modules/movies/routes/routes.js
+++ src/modules/movies/routes/routes.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { client } from "../../../mongo.js";
 
 const route = express.Router();
 
-route.get("/", (req, res) => {
-  res.send("movies");
+const database = client.db("sample_mflix");
+const movies_collection = database.collection("movies");
+
+route.get("/title", async (req, res) => {
+  const { title } = req.query;
+
+  if (!title) {
+    res.json({ success: false, message: "title required" });
+  }
+
+  const movie = await movies_collection.findOne({ title: { $eq: title } });
+
+  res.json({ success: true, data: { movie } });
 });
 
 export { route };
