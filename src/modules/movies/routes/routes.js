0455fa556b4929c1d96b5dee0e04670a83283999
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-28 18:33:44 +0800

    task2

--- src/modules/movies/routes/routes.js
+++ src/modules/movies/routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { client } from "../../../mongo.js";
+import { parse } from "dotenv";
 
 const route = express.Router();
 
@@ -16,6 +17,33 @@ route.get("/title", async (req, res) => {
   const movie = await movies_collection.findOne({ title: { $eq: title } });
 
   res.json({ success: true, data: { movie } });
+});
+
+route.get("/genre", async (req, res) => {
+  const { genre, page = 1, limit = 10 } = req.query;
+
+  const parsedPage = parseInt(page);
+  const parsedLimit = parseInt(limit);
+
+  if (!genre) {
+    res.json({ success: false, message: "genre required" });
+  }
+
+  const query = { genres: { $eq: genre } };
+
+  const movies = await movies_collection
+    .find(query)
+    .skip((parsedPage - 1) * parsedLimit)
+    .limit(parsedLimit)
+    .toArray();
+
+  const movieCount = await movies_collection.countDocuments(query);
+
+  res.json({
+    success: true,
+    pagination: { total: movieCount, page, limit, totalReturn: limit },
+    data: { movies }
+  });
 });
 
 export { route };
